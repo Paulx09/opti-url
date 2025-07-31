7b5027c27a3a3b9239edf758ccca136a3a6bbc30
PaulxA09 <pauloarmey18@gmail.com>
2025-07-30 19:00:49 -0500

    feat: add MySQL database configuration to Flask app

--- src/app.py
+++ src/app.py
@@ -8,6 +8,15 @@ app = Flask(__name__)
 # endpoint
 endpoint = 'http://opti.url'
 
+# connect mysql
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'P@u102018.180905'
+app.config['MYSQL_DATABASE'] = 'db_opti_url'
+
+# init mysql
+mysql = MySQL(app)
+
 # run app
 if __name__ == '__main__':
     app.run(port = 80, debug = True)
